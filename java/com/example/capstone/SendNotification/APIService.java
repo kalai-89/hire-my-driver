867317cff154efea59a33477802e11bd61e286d7
kalai-89 <169015624+kalai-89@users.noreply.github.com>
2024-05-06 10:05:36 +0530

    Update APIService.java

--- java/com/example/capstone/SendNotification/APIService.java
+++ java/com/example/capstone/SendNotification/APIService.java
@@ -10,7 +10,7 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type:application/json",
-                    "Authorization:key=AAAAmEOviYY:APA91bHypdSCA5FIUtwsqYFDDOJRbT3PuROMjp-OH-DoaIgFkA915KJoAmZUJuGC8Ovv5_YA56bOhyVVuZt3SghkXS83l6n7qpnva1hc9sc0Qr4ZpvaOK723tBfMeBPgoFem0b-OfvET"
+                    "Authorization:key= "
             }
     )
 
